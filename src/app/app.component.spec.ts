b0881a6339ceada2e4401e7c1fffcb6479ecbfb3
rockan007 <rockan007@gmail.com>
2020-09-11 17:28:53 +0800

    更改系统名称， 添加 左侧功能列表在PC端也可隐藏功能

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tool-map'`, () => {
+  it(`should have as title 'tools'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tool-map');
+    expect(app.title).toEqual('tools');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tool-map app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tools app is running!');
   });
 });

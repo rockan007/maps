6be1766bde3e2502b003d7b5c8ae47a188fcfa80
rockan007 <rockan007@gmail.com>
2020-09-19 15:28:05 +0800

    将项目名称更改为tools

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tool-map app is running!');
+    expect(page.getTitleText()).toEqual('tools app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/tool-map'),
+      dir: require('path').join(__dirname, './coverage/tools'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },

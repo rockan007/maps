f7e1f05aa1aaa9877f8df61a841f390b84d44ac1
Andy An <rockan007@gmail.com>
2024-04-08 20:39:05 +0800

    [feature]默认路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'charts-map', loadChildren: () =>  import('./charts-map/charts-map.module').then(m => m.ChartsMapModule) },
-  { path: 'sky-map', loadChildren: () => import('./sky-map/sky-map.module').then(m => m.SkyMapModule)},
+  { path: 'charts-map', loadChildren: () => import('./charts-map/charts-map.module').then(m => m.ChartsMapModule) },
+  { path: 'sky-map', loadChildren: () => import('./sky-map/sky-map.module').then(m => m.SkyMapModule) },
+  { path: '', redirectTo: 'charts-map', pathMatch: 'full' }
 ];
 
 @NgModule({
